ef74e4282030adfe7612ddeb44377d8cb92c53be
christiancarlmacan <christiancarlmacan@gmail.com>
2025-11-18 20:45:20 +0800

    Cleanup

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,105 +1,138 @@
-'use client'
-
-import { z } from "zod"
-import { useForm } from "react-hook-form"
-import { useRouter } from "next/navigation"
-import Link from "next/link"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { Button } from "@/components/ui/button"
-import { Checkbox } from "@/components/ui/checkbox"
-import { Label } from "@/components/ui/label"
+"use client";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 interface loginState {
-    studentid: string
-    password: string
+  studentid: string;
+  password: string;
 }
 
 const formSchema = z.object({
-    studentid: z.string()
-        .regex(/[0-9]/, "Student ID should only contain numbers."),
-    password: z.string()
-        .regex(/[A-Z]/, "Password should contain at least 1 uppercase letter")
-        .regex(/[0-9]/, "Password should contain at least 1 number")
-        .regex(/^\S*$/, "Password should not contain any spaces")
-})
+  studentid: z
+    .string()
+    .regex(/[0-9]/, "Student ID should only contain numbers."),
+  password: z
+    .string()
+    .regex(/[A-Z]/, "Password should contain at least 1 uppercase letter")
+    .regex(/[0-9]/, "Password should contain at least 1 number")
+    .regex(/^\S*$/, "Password should not contain any spaces"),
+});
 
 export default function login() {
-    const router = useRouter()
-    const form = useForm<loginState>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            studentid: "",
-            password: ""
-        }
-    })
+  const router = useRouter();
+  const form = useForm<loginState>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      studentid: "",
+      password: "",
+    },
+  });
 
-    const handleSubmit = () => {
-        router.push("/home")
-    }
+  const handleSubmit = () => {
+    router.push("/home");
+  };
 
-    return (
-        <div className="w-screen h-screen flex items-center justify-center bg-[rgb(50,70,70)]">
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-3 rounded-3xl px-15 p-10 w-130 flex flex-col items-center bg-[rgb(200,220,220)]">
-                    <div className="flex flex-col items-center mb-10">
-                        <img className="w-15 mb-3" src="globe.svg" alt="logo" />
-                        <h1 className="text-center text-3xl font-extrabold tracking-tight">
-                            Login
-                        </h1>
-                        <p className="text-muted-foreground text-sm">
-                            Your Gateway to Knowledge
-                        </p>
-                    </div>
-                    <div className="w-full space-y-3">
-                        <FormField
-                            control={form.control}
-                            name="studentid"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel className="text-md font-mediumbold">Student ID</FormLabel>
-                                    <FormControl>
-                                        <Input type="text" className="bg-white" placeholder="Enter your Student ID" maxLength={6} {...field} />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            control={form.control}
-                            name="password"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel className="text-md font-mediumbold">Password</FormLabel>
-                                    <FormControl>
-                                        <Input type="password" className="bg-white" placeholder="Enter your Password" {...field} />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                    </div>
-                    <div className="w-full flex justify-between">
-                        <div className="flex gap-2 items-center">
-                            <Checkbox className="bg-white" id="remember" />
-                            <Label htmlFor="remember">Remember Me</Label>
-                        </div>
-                        <Link href="/forgotpassword"><small className="text-sm leading-none font-medium text-green-700">Forgot Password?</small></Link>
-                    </div>
-                    <Button className="bg-blue-700 rounded-full text-lg px-10 hover:bg-blue-600 cursor-pointer" type="submit">Login</Button>
-                    <div className="w-full flex justify-center mt-3">
-                        <small className="text-sm leading-none font-medium">Don't have an account? <Link className="text-green-700" href="/signup">Sign up</Link></small>
-                    </div>
-                </form>
-            </Form>
-        </div>
-    )
-}git 
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-[rgb(50,70,70)]">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleSubmit)}
+          className="flex w-130 flex-col items-center space-y-3 rounded-3xl bg-[rgb(200,220,220)] p-10 px-15"
+        >
+          <div className="mb-10 flex flex-col items-center">
+            <img className="mb-3 w-15" src="globe.svg" alt="logo" />
+            <h1 className="text-center text-3xl font-extrabold tracking-tight">
+              Login
+            </h1>
+            <p className="text-muted-foreground text-sm">
+              Your Gateway to Knowledge
+            </p>
+          </div>
+          <div className="w-full space-y-3">
+            <FormField
+              control={form.control}
+              name="studentid"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-md font-mediumbold">
+                    Student ID
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      type="text"
+                      className="bg-white"
+                      placeholder="Enter your Student ID"
+                      maxLength={6}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-md font-mediumbold">
+                    Password
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      className="bg-white"
+                      placeholder="Enter your Password"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="flex w-full justify-between">
+            <div className="flex items-center gap-2">
+              <Checkbox className="bg-white" id="remember" />
+              <Label htmlFor="remember">Remember Me</Label>
+            </div>
+            <Link href="/forgotpassword">
+              <small className="text-sm leading-none font-medium text-green-700">
+                Forgot Password?
+              </small>
+            </Link>
+          </div>
+          <Button
+            className="cursor-pointer rounded-full bg-blue-700 px-10 text-lg hover:bg-blue-600"
+            type="submit"
+          >
+            Login
+          </Button>
+          <div className="mt-3 flex w-full justify-center">
+            <small className="text-sm leading-none font-medium">
+              {"Don't have an account?"}{" "}
+              <Link className="text-green-700" href="/signup">
+                Sign up
+              </Link>
+            </small>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
+}
--- client/app/signup/page.tsx
+++ client/app/signup/page.tsx
@@ -1,129 +1,175 @@
-'use client'
-
-import { z } from "zod"
-import { useForm } from "react-hook-form"
-import { useRouter } from "next/navigation"
-import Link from "next/link"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { Button } from "@/components/ui/button"
-import { MoveLeft } from "lucide-react"
+"use client";
+import { Button } from "@/components/ui/button";
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { MoveLeft } from "lucide-react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 interface signupState {
-    name: string
-    studentid: string
-    email: string
-    password: string
+  name: string;
+  studentid: string;
+  email: string;
+  password: string;
 }
 
 const formSchema = z.object({
-    name: z.string(),
-    studentid: z.string()
-        .regex(/[0-9]/, "Student ID should only contain numbers."),
-    email: z.string(),
-    password: z.string()
-        .regex(/[A-Z]/, "Password should contain at least 1 uppercase letter")
-        .regex(/[0-9]/, "Password should contain at least 1 number")
-        .regex(/^\S*$/, "Password should not contain any spaces")
-})
+  name: z.string(),
+  studentid: z
+    .string()
+    .regex(/[0-9]/, "Student ID should only contain numbers."),
+  email: z.string(),
+  password: z
+    .string()
+    .regex(/[A-Z]/, "Password should contain at least 1 uppercase letter")
+    .regex(/[0-9]/, "Password should contain at least 1 number")
+    .regex(/^\S*$/, "Password should not contain any spaces"),
+});
 
 export default function signup() {
-    const router = useRouter()
-    const form = useForm<signupState>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            name: "",
-            studentid: "",
-            email: "",
-            password: ""
-        }
-    })
+  const router = useRouter();
+  const form = useForm<signupState>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      studentid: "",
+      email: "",
+      password: "",
+    },
+  });
 
-    const handleSubmit = () => {
-        router.push("/home")
-    }
+  const handleSubmit = () => {
+    router.push("/home");
+  };
 
-    return (
-        <div className="w-screen h-screen flex items-center justify-center bg-[rgb(50,70,70)]">
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-3 rounded-3xl px-15 p-10 w-130 flex flex-col items-center bg-[rgb(200,220,220)]">
-                    <div className="flex flex-col items-center mb-10">
-                        <img className="w-15 mb-3" src="globe.svg" alt="logo" />
-                        <h1 className="text-center text-3xl font-extrabold tracking-tight">
-                            User Sign Up
-                        </h1>
-                        <p className="text-muted-foreground text-sm">
-                            Your journey to Knowledge begins here
-                        </p>
-                    </div>
-                    <div className="w-full space-y-3">
-                        <FormField
-                            control={form.control}
-                            name="name"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel className="text-md font-mediumbold">Name</FormLabel>
-                                    <FormControl>
-                                        <Input type="text" className="bg-white" placeholder="Enter your Fullname" {...field} />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            control={form.control}
-                            name="studentid"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel className="text-md font-mediumbold">Student ID</FormLabel>
-                                    <FormControl>
-                                        <Input type="text" className="bg-white" placeholder="Enter your Student ID" maxLength={6} {...field} />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            control={form.control}
-                            name="studentid"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel className="text-md font-mediumbold">Email</FormLabel>
-                                    <FormControl>
-                                        <Input type="text" className="bg-white" placeholder="Enter your Email" {...field} />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            control={form.control}
-                            name="password"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel className="text-md font-mediumbold">Password</FormLabel>
-                                    <FormControl>
-                                        <Input type="password" className="bg-white" placeholder="Enter your password" {...field} />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                    </div>
-                    <Button className="mt-5 bg-blue-700 rounded-full text-lg px-10 hover:bg-blue-600 cursor-pointer" type="submit">Sign Up</Button>
-                    <div className="w-full flex items-center justify-center gap-2 mt-3">
-                        <small className="text-sm leading-none font-medium"><Link className="text-green-700 flex items-center justify-center gap-2" href="/login"><MoveLeft color="#3c9d2f" size={10} strokeWidth={3} />Back to Login</Link></small>
-                    </div>
-                </form>
-            </Form>
-        </div>
-    )
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-[rgb(50,70,70)]">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleSubmit)}
+          className="flex w-130 flex-col items-center space-y-3 rounded-3xl bg-[rgb(200,220,220)] p-10 px-15"
+        >
+          <div className="mb-10 flex flex-col items-center">
+            <img className="mb-3 w-15" src="globe.svg" alt="logo" />
+            <h1 className="text-center text-3xl font-extrabold tracking-tight">
+              User Sign Up
+            </h1>
+            <p className="text-muted-foreground text-sm">
+              Your journey to Knowledge begins here
+            </p>
+          </div>
+          <div className="w-full space-y-3">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-md font-mediumbold">
+                    Name
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      type="text"
+                      className="bg-white"
+                      placeholder="Enter your Fullname"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="studentid"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-md font-mediumbold">
+                    Student ID
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      type="text"
+                      className="bg-white"
+                      placeholder="Enter your Student ID"
+                      maxLength={6}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="studentid"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-md font-mediumbold">
+                    Email
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      type="text"
+                      className="bg-white"
+                      placeholder="Enter your Email"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-md font-mediumbold">
+                    Password
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      className="bg-white"
+                      placeholder="Enter your password"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button
+            className="mt-5 cursor-pointer rounded-full bg-blue-700 px-10 text-lg hover:bg-blue-600"
+            type="submit"
+          >
+            Sign Up
+          </Button>
+          <div className="mt-3 flex w-full items-center justify-center gap-2">
+            <small className="text-sm leading-none font-medium">
+              <Link
+                className="flex items-center justify-center gap-2 text-green-700"
+                href="/login"
+              >
+                <MoveLeft color="#3c9d2f" size={10} strokeWidth={3} />
+                Back to Login
+              </Link>
+            </small>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
 }
